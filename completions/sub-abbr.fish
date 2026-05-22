c2362cdc9d04fd5465e5b2ab0935743bc4c0b33f
Drazape <Drazape@outlook.com>
2026-05-22 10:45:30 +0530

    completions: common: alias → var

--- completions/sub-abbr.fish
+++ completions/sub-abbr.fish
@@ -1,8 +1,8 @@
-alias common-complete 'complete --command=sub-abbr --no-files'
+set --local -- common_complete complete --command=sub-abbr --no-files
 
-common-complete
-common-complete --short-option=h --long-option=help --description=Help
-common-complete --short-option=c --long-option=set-cursor --description='Position the cursor at % post-expansion'
-common-complete --short-option=0 --long-option=degrade --description='don\'t tolerate run0 prefix'
-common-complete --short-option=s --long-option=regard-flags --description='Acknowledge flags in the Initial Command'
-common-complete --short-option=f --long-option=function --description='Use the output of a command as the Expansion'
+$common_complete
+$common_complete --short-option=h --long-option=help --description=Help
+$common_complete --short-option=c --long-option=set-cursor --description='Position the cursor at % post-expansion'
+$common_complete --short-option=0 --long-option=degrade --description='don\'t tolerate run0 prefix'
+$common_complete --short-option=s --long-option=regard-flags --description='Acknowledge flags in the Initial Command'
+$common_complete --short-option=f --long-option=function --description='Use the output of a command as the Expansion'
