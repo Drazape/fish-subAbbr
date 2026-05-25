4785d98126e41be2e1e9ace711e8e31b80f9f701
Drazape <Drazape@outlook.com>
2026-05-25 09:24:33 +0530

    expand-subcommand: organise `arg_count` order in appropriate sections

--- functions/_sub-abbr_expand-subcommand.fish
+++ functions/_sub-abbr_expand-subcommand.fish
@@ -11,11 +11,11 @@ function _sub-abbr_expand-subcommand --description='Expand a subcommand'
     # Commandline
     set --local argv (commandline --tokens-expanded --current-process)[..-2]
     set --local --query _flag_regard_flags || argparse --move-unknown -- {$argv}
-    set --function arg_count (count {$initial_args})
     set --function active_sub_args {$argv[2..]}
     ! set --local --query _flag_degrade && test {$argv[1]} = run0 && set --function active_sub_args {$active_sub_args[2..]} # Remove real Base Command from sub arguments
 
     # Compare
+    set --local arg_count (count {$initial_args})
     test {$arg_count} -eq (count {$active_sub_args}) || return 1
     for i in (seq 1 {$arg_count})
         test {$initial_args[$i]} = {$active_sub_args[$i]} || return 2
