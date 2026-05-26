09ab2e1c448fcb4b6ba0430491fc96ff33d8c3cf
Drazape <Drazape@outlook.com>
2026-05-26 10:08:28 +0530

    pkg: nh: rework: looped abbrs → flags

--- functions/sub-abbr/pkg/nh.fish
+++ functions/sub-abbr/pkg/nh.fish
@@ -3,8 +3,6 @@ begin
     set --local exec_section _{$exec_name}
     function _sub-abbr{$exec_section} --description='context-aware expansions for nh' --on-event=sub-abbr{s,$exec_section} --inherit-variable=exec_name
         # os: allow root
-        for subCommand in switch boot build{,-image,-vm} rollback test
-            sub-abbr add --degrade -- run0 {$exec_name} os {$subCommand}{,\ --bypass-root-check}
-        end
+        sub-abbr add --degrade --expander --regex=sub-command -- run0 {$exec_name} os '(switch|boot|build\-image|build\-vm|rollback|test)' '_sub-abbr_lib_expander_append --bypass-root-check'
     end
 end
