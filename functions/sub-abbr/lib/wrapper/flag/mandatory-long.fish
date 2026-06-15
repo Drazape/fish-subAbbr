48a82a53ef7a5f08ae70f3f8bb947d7ec8a8dd4e
Drazape <Drazape@outlook.com>
2026-06-15 15:31:35 +0530

    style: wrapper: flag: mandatory-long: explain join with `|`

--- functions/sub-abbr/lib/wrapper/flag/mandatory-long.fish
+++ functions/sub-abbr/lib/wrapper/flag/mandatory-long.fish
@@ -3,7 +3,7 @@ function _sub-abbr_lib_wrapper_flag_mandatory-long --description='Enforce input 
     set --query --local -- _flag_flag || return 1
 
     set --function -- long_flag (string escape --style=regex -- {$_flag_flag})
-    test (count {$_flag_flag}) != 1 && set --function -- long_flag \("$(string join -- \| {$long_flag})"\)
+    test (count {$_flag_flag}) != 1 && set --function -- long_flag \("$(string join -- \| {$long_flag})"\) # If multiple flags are specified, split them with or.
 
     set --local -- cursor (status current-function)_cursor
     sub-abbr add --expander --set-cursor={$cursor} --regex=sub-command {$argv_opts} -- {$argv} '\-\-'{$long_flag} _sub-abbr_internal_expander_flag_mandatory-long\ {$cursor}
