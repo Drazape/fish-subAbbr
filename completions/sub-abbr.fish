bd06cf8b5501084d3f8a97b25422e30ac5b35f9e
Drazape <Drazape@outlook.com>
2026-06-16 10:45:15 +0530

    feat: completions: sub-abbr: no duplicate suggestions
    Don't suggest duplicate suggestions -unless they can be *meaningfully* used again

--- completions/sub-abbr.fish
+++ completions/sub-abbr.fish
@@ -1,9 +1,16 @@
 set --local -- common_complete complete --command=sub-abbr --no-files
+function single-switch --description='Only suggest the switch once' --inherit-variable=common_complete
+    argparse --move-unknown s/short-option= l/long-option= -- {$argv}
+    $common_complete --condition='! __fish_seen_argument --short='{$_flag_short_option}' --long='{$_flag_long_option} {$argv_opts} -- {$argv}
+end
 
 $common_complete
-$common_complete --short-option=h --long-option=help --description='Reference manuals' \
+single-switch --short-option=h --long-option=help --description='Reference manuals' \
     --condition='set --local -- unbase (commandline --tokens-expanded --current-process --cut-at-cursor)[2..3]
-                test "$unbase[1]" = identity && test "$unbase[2]" != list'
+                 test (count {$unbase}) -eq 0 && return 0
+                 test "$unbase[1]" = add && return 0
+                 test "$unbase[1]" = identity && test "$unbase[2]" != list && return 0
+                 return 1'
 
 set --local -- subcommand_complete {$common_complete} --condition='test (count (commandline -xpc)) -lt 2'
 $subcommand_complete --arguments=add --description='Create abbrs'
@@ -17,15 +24,17 @@ $identity_complete --arguments=erase --description='Erase abbrs with identity'
 $common_complete \
     --condition='set --local -- subcommands (commandline --tokens-expanded --current-process --cut-at-cursor)[2..3]
                 test "$subcommands[1]" = identity && test "$subcommands[2]" = erase' \
-    --arguments='(sub-abbr identity list)'
+    --arguments='(sub-abbr identity list | string match --invert --regex -- (string escape --style=regex -- (commandline --tokens-expanded --current-process) | string join -- \|))'
 
-set --local -- creation_complete {$common_complete} --condition='test "$(commandline -xpc)[2]" = add'
+set --local -- creation_condition --condition='test "$(commandline -xpc)[2]" = add'
+
+set --local -- creation_complete single-switch {$creation_condition}
 $creation_complete --short-option=c --long-option=set-cursor --description='Position the cursor at % post-expansion'
 $creation_complete --short-option=0 --long-option=degrade --description='don\'t tolerate run0 prefix'
 $creation_complete --short-option=s --long-option=regard-flags --description='Acknowledge flags in the Initial Command'
 $creation_complete --short-option=e --long-option=expander --description='Use the output of a command as the Expansion'
 
-set --local regex_complete {$creation_complete} --short-option=r --long-option=regex
+set --local regex_complete {$common_complete} {$creation_condition} --short-option=r --long-option=regex
 $regex_complete --description='Match command-line arguments with RegExp'
 set --local -- regex_value {$regex_complete} --condition='string match --quiet --regex -- \'^(--regex=|-r)\w*$\' (commandline -xtc)'
 $regex_value --arguments=sub-command --description='Match the sub-command with RegExp'
