f8e380bef636fba71deffe2a6eeec8f5dda9c300
Drazape <Drazape@outlook.com>
2026-06-18 09:08:54 +0530

    refactor(completions): `functions`: Remove string parsing
    - When the output listing functions from `functions` is piped to STDOUT, it is seperated with `, `, but when piped elsewhere, it is sepereated with newline. Because of this, there is no need to split the string with `, `
    - When getting the function definition, there is no need to use `string join0` and `string split0 --fields=1` to get the output. Since the text is a command substitution, we can get the definition line (first line) directly with `[1]`

--- completions/sub-abbrs.fish
+++ completions/sub-abbrs.fish
@@ -1,8 +1,8 @@
 set --local -- common_complete complete --command=sub-abbrs --no-files
 $common_complete
 
-for function in (string match --regex --entire -- '^_sub-abbr_pkg_' (path basename --no-extension -- {$fish_function_path}/*) (functions --all | string split -- ,\ ))
-    argparse --ignore-unknown /description=+\& -- (commandline --tokens-expanded --input=(functions --no-details -- {$function} | string join0 | string split0 --fields=1))
+for function in (string match --regex --entire -- '^_sub-abbr_pkg_' (path basename --no-extension -- {$fish_function_path}/*) (functions --all))
+    argparse --ignore-unknown /description=+\& -- (commandline --tokens-expanded --input=(functions --no-details -- {$function})[1])
 
     set --local -- pkg (string split --fields=2 -- _sub-abbr_pkg_ {$function})
     $common_complete --description={$_flag_description} \
