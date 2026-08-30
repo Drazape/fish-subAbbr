9b17a09d8b39204236aa04d4e2fe14e9cdd065f6
Drazape <Drazape@outlook.com>
2026-08-30 19:00:39 +0530

    style(pipe): sub-abbrs: separate long parsing into a new line

--- functions/sub-abbrs.fish
+++ functions/sub-abbrs.fish
@@ -1,6 +1,7 @@
 function sub-abbrs --description='Package repository helper for sub-abbrs'
     test (count {$argv}) -eq 0 && emit sub-abbrs
-    for repo_func in (string match --entire --regex -- ^_sub-abbr_pkg_"$(string join -- _ {$argv})" (string match --entire --regex -- ^_sub-abbr_pkg_ (functions --all) (path basename --no-extension {$fish_function_path}/*)))
+    for repo_func in (string match --entire --regex -- ^_sub-abbr_pkg_ (functions --all) (path basename --no-extension {$fish_function_path}/*) |
+            string match --entire --regex -- ^_sub-abbr_pkg_"$(string join -- _ {$argv})")
         $repo_func
     end
 end
