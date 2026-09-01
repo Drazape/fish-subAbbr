5b836447a978742f4ed4e7dc6eff1e0278a6056c
Drazape <Drazape@outlook.com>
2026-09-01 09:09:28 +0530

    perf(helper): no manual funcpath matching
    Previously, `sub-abbrs` would also manually match each function path manually -in addition to having Fish do that.
    This would result in exact duplicate results, since Fish already includes the functions from indexing the `$fish_function_path` paths.

--- functions/sub-abbrs.fish
+++ functions/sub-abbrs.fish
@@ -1,6 +1,6 @@
 function sub-abbrs --description='Package repository helper for sub-abbrs'
     test (count {$argv}) -eq 0 && emit sub-abbrs
-    for repo_func in (string match --entire --regex -- ^_sub-abbr_pkg_ (functions --all) (path basename --no-extension {$fish_function_path}/*) |
+    for repo_func in (functions --all | string match --entire --regex -- ^_sub-abbr_pkg_ |
             string match --entire --regex -- ^_sub-abbr_pkg_"$(string join -- _ {$argv})")
         $repo_func
     end
