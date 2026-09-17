5b2342e97713da5bd3494c53dd2ee6a8fc712c8d
Drazape <Drazape@outlook.com>
2026-09-17 19:23:05 +0530

    fix(flags): value: regex: solve misc verification issues

--- functions/sub-abbr/internal/verify-arg/regex-val.fish
+++ functions/sub-abbr/internal/verify-arg/regex-val.fish
@@ -1,4 +1,7 @@
 function _sub-abbr_internal_verify-arg_regex-val --description='Verify that the RegExp flag gets one of the appropriate values'
-    set --local -- accepted_values sub-command initials
-    contains {$_flag_value} {$accepted_values} || echo 'Expected value one of:' (format background brblack --bright {$accepted_values})
+    set --local -- regex_matchable_args sub-command initials
+    if ! contains -- {$_flag_value} {$regex_matchable_args}
+        echo (format text dim 'sub-abbr')(format text dim (format text color white ':')) 'Expected value among:' (format background black --bright {$regex_matchable_args})
+        return 1
+    end
 end
