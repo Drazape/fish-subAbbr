386314857ff775e76a8c176a8ada1b6c54d2d532
Drazape <Drazape@outlook.com>
2026-09-19 17:49:01 +0530

    perf(helper): remove repetitive match
    Remove the individual matching of the explicit filteration of abbreviations that are strictly context-aware sub-command abbrevaitions.  
    The same check is later peformed when comparing with the passed package path in the package tree.

--- functions/sub-abbrs.fish
+++ functions/sub-abbrs.fish
@@ -1,7 +1,6 @@
 function sub-abbrs --description='Package repository helper for sub-abbrs'
     test (count {$argv}) -eq 0 && emit sub-abbrs
-    for repo_func in (functions --all | string match --entire --regex -- ^_sub-abbr_pkg_ |
-            string match --entire --regex -- ^_sub-abbr_pkg_"$(string join -- _ {$argv})")
+    for repo_func in (functions --all | string match --entire --regex -- ^_sub-abbr_pkg_"$(string join -- _ {$argv})")
         $repo_func
     end
 end
